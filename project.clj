4e2b09a1c383dfe09b56e9eeb5a4d0c7782071f5
Junyu Lu <jy.lu@outlook.com>
2020-01-07 11:18:04 +0800

    update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject yleisradio/new-reliquary "1.1.0"
+(defproject hinamizawa/new-reliquary "0.1.0"
   :description "Clojure newrelic java api wrapper"
-  :url "https://github.com/Yleisradio/new-reliquary"
+  :url "https://github.com/hinamizawa/new-reliquary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
