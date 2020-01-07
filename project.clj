0dc11cdc0bd3d8b6093618ea7f25be21a4f6808a
Junyu Lu <jy.lu@outlook.com>
2020-01-07 10:44:24 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,8 @@
   :url "https://github.com/Yleisradio/new-reliquary"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.newrelic.agent.java/newrelic-api "4.0.0"]]
-  :profiles { :dev { :dependencies [[ring/ring-core "1.6.3"]
-                                    [ring-mock "0.1.5"]
-                                    [org.clojure/tools.trace "0.7.9"]]}}
-  :plugins [[lein-ancient "0.6.14"]]
-  :signing {:gpg-key "D9A17928"})
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.newrelic.agent.java/newrelic-api "5.9.0"]]
+  :profiles { :dev { :dependencies [[ring/ring-mock "0.4.0"]
+                                    [ring "1.8.0"]
+                                    [org.clojure/tools.trace "0.7.10"]]}})
