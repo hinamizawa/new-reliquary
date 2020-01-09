9ee4de2d2909fbaf503d26e7bfb3fe2a06f4b9aa
Junyu Lu <jy.lu@outlook.com>
2020-01-09 13:07:06 +0800

    add ring-compojure suitable middleware fn

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hinamizawa/new-reliquary "0.1.2"
+(defproject hinamizawa/new-reliquary "0.1.3"
   :description "Clojure newrelic java api wrapper"
   :url "https://github.com/hinamizawa/new-reliquary"
   :license {:name "Eclipse Public License"
